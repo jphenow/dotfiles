623b0c78487ac22134f0971f2984013364e8e4c7
Jon Phenow <j.phenow@gmail.com>
2011-10-31 15:36:13 -0500

    Fixed order of things in config.bash so we still have vim editting in command line, and also allowed to use our fun autocomplete commands based on history. See bash/config.bash.

--- bash/config.bash
+++ bash/config.bash
@@ -1,26 +1,9 @@
-# History
-shopt -s histappend
-HISTFILESIZE=100000000
-HISTSIZE=100000
-PROMPT_COMMAND='history -a'
-
-# don't put duplicate lines in the history. See bash(1) for more options
-export HISTCONTROL=ignoredups
-
-
-# Nice 
-bind '"\C-s":menu-complete'
-bind '"\C-f":history-search-backward'
-
 # Important to some of us
 set -o vi
 export EDITOR=/usr/bin/vi
 
-
-# set variable identifying the chroot you work in (used in the prompt below)
-if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
-    debian_chroot=$(cat /etc/debian_chroot)
-fi
+# don't put duplicate lines in the history. See bash(1) for more options
+export HISTCONTROL=ignoredups
 
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
@@ -28,3 +11,18 @@ shopt -s checkwinsize
 
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(lesspipe)"
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+# History
+shopt -s histappend
+HISTFILESIZE=100000000
+HISTSIZE=100000
+PROMPT_COMMAND='history -a'
+
+# Nice
+#bind '"\C-s": menu-complete'
+bind '"\C-f": history-search-backward'
