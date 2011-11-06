d5667fbd9cdc83e502a65a1541e9ed13e4512f39
Jon Phenow <j.phenow@gmail.com>
2011-11-06 14:08:22 -0600

    Finally found the key to stopping new-line issues because of prompt issues. Could still use some cleanup, but it works cleanly now

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -2,12 +2,15 @@
 # Mine includes Goodness like:
 #  * Colors
 #  * Git branch name
-RESET="\033[0m"
-R="\033[0;31m"
-G="\033[0;32m"
-M="\033[0;35m"
-Y="\033[0;33m"
-git_dirty() {
+RESET="\e[0m"
+R="\e[0;31m"
+G="\e[0;32m"
+M="\e[0;35m"
+Y="\e[0;33m"
+DG="\e[1;34m"
+DR="\e[1;31m"
+D="\e[1;32m"
+git_color() {
   if [[ $GITPROMPT == true ]]
   then
     st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
@@ -17,9 +20,9 @@ git_dirty() {
     else
       if [[ $st == "nothing to commit (working directory clean)" ]]
       then
-        echo -ne "${G}$(git_ps1)${RESET}"
+        echo -e "$G"
       else
-        echo -ne "${R}$(git_ps1)${RESET}"
+        echo -e "$R"
       fi
     fi
   fi
@@ -29,9 +32,9 @@ git_dirty() {
 rvm_prompt(){
   if $(which rvm &> /dev/null)
   then
-	  echo -ne "${Y}$(rvm tools identifier)${RESET}"
-	else
-	  echo ""
+    echo -e "${Y}$(rvm tools identifier)${RESET}"
+  else
+    echo ""
   fi
 }
 
@@ -60,15 +63,14 @@ todo(){
 }
 git_ps1 ()
 {
-	__git_ps1 1>/dev/null 2>/dev/null
-	err=$?
-	if [ "$err" == "0" ]; then
-		ps=$( __git_ps1 "(%s)" )
-		echo -ne "$ps"
-	else
-		echo -ne ""
-	fi
+  __git_ps1 1>/dev/null 2>/dev/null
+  err=$?
+  if [ "$err" == "0" ]; then
+      echo -e "$(__git_ps1)"
+  fi
 }
+
+trim() { echo $1; }
 
 # You want this here
 use_color=false
@@ -81,11 +83,15 @@ use_color=false
 # TODO Prompt config looks like ass - cleanup
 safe_term=${TERM//[^[:alnum:]]/?}   # sanitize TERM
 match_lhs=""
+
 [[ -f $DOT/colors/dir_colors   ]] && match_lhs="${match_lhs}$(<$DOT/colors/dir_colors)"
+
 [[ -f /etc/DIR_COLORS ]] && match_lhs="${match_lhs}$(</etc/DIR_COLORS)"
+
 [[ -z ${match_lhs}    ]] \
   && type -P dircolors >/dev/null \
   && match_lhs=$(dircolors --print-database)
+
 [[ $'\n'${match_lhs} == *$'\n'"TERM "${safe_term}* ]] && use_color=true
 
 if ${use_color} ; then
@@ -99,9 +105,9 @@ if ${use_color} ; then
   fi
 
   if [[ ${EUID} == 0 ]] ; then
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\h\[\033[0;36m\]$(git_dirty "%s") \[\033[01;34m\] \W$(todo)\[\033[00m\] '
+    PS1="${debian_chroot:+($debian_chroot)}\[$DR\]\h[$(git_color)\]$(trim $(git_ps1)) \[$DG\] \W\[$(todo)\]\[$RESET\] "
   else
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h$(git_dirty "%s") \[\033[01;34m\]\w$(todo)\[\033[00m\] '
+    PS1="${debian_chroot:+($debian_chroot)}\[$D\]\u@\h\[$(git_color)\]$(trim $(git_ps1)) \[$DG\]\w$(todo)\[$RESET\] "
   fi
 
   alias ls='ls --color=auto'
