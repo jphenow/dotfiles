ade3bfcf7300373ffc0876623e7fca79db4c9a9c
Jon Phenow <j.phenow@gmail.com>
2011-11-14 22:54:47 -0600

    Made grabbing config bashes and completion.sh prettier Moved most of the bash aliases to system folder because it makes more sense Added a reload! alias for reloading bash

--- system/aliases.bash
+++ system/aliases.bash
@@ -1,3 +1,15 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
+alias grep='grep --colour=auto'
+alias ls='ls -F --color'
+alias ll='ls -l'
+alias la='ls -A'
+alias l='ls -CF'
+alias s='ls'
+alias a='cd ..'
+alias c='cd'
+alias v='vim .'
+alias vo='vim -o '
+alias va='vo *'
+alias ir='irb --noprompt --readline -r irb/completion'
+alias irb='irb --readline -r irb/completion'
+alias acroread='acroread -openInNewWindow'
+alias date_for_filename='date +%Y-%m-%d_%H-%M-%S'
