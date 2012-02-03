e31b621ec417dfa0cb333e5ca1f72f6af735651a
Jon Phenow <j.phenow@gmail.com>
2012-02-02 21:10:51 -0600

    some paths and aliases that are useful since mac switch

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,5 @@
-export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$DOT/bin:$PATH"
+export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$DOT/bin:/usr/texbin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export NODE_PATH="/usr/local/lib/node_modules:$NODE_PATH"
