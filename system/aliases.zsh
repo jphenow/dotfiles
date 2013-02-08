63ad38f99970782a968f659c0c86f4195cbf8b97
Jon Phenow <j.phenow@gmail.com>
2013-02-07 19:06:38 -0600

    hide some errors, fix colors, fix command refs

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,6 +1,7 @@
 # grc overides for ls
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
+
 if $(gls &>/dev/null)
 then
   alias ls="gls -F --color"
@@ -8,19 +9,29 @@ then
   alias ll="gls -Fl --color"
   alias la='gls -lAH --color'
 else
-  alias ls='ls -FCG'
+  alias ls='ls --color -FCG'
   alias l='ls -CF'
   alias ll='ls -lh'
   alias la='ls -A'
 fi
 
+vimcmd=""
+
+if $(mvim &>/dev/null)
+then
+  vimcmd="mvim"
+else
+  vimcmd="vim"
+fi
+
+alias vim="$vimcmd -v -o"
+alias v="$vimcmd -v ."
+alias va="$vimcmd -v -o *"
+
 alias grep='grep --colour=auto'
 alias s='ls'
 alias a='cd ..'
 alias c='cd'
-alias vim='mvim -v -o'
-alias v='mvim -v .'
-alias va='mvim -v -o *'
 alias ir='irb --noprompt --readline -r irb/completion'
 alias irb='irb --readline -r irb/completion'
 alias date_for_filename='date +%Y-%m-%d_%H-%M-%S'
