f6c90db96538a401043ccb6b4abe33230653f618
Jon Phenow <j.phenow@gmail.com>
2013-09-04 09:13:16 -0500

    new theme + syntax highlighting

--- oh-my-zsh/oh-my-zsh.zsh
+++ oh-my-zsh/oh-my-zsh.zsh
@@ -5,7 +5,8 @@ ZSH=$HOME/.oh-my-zsh
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-export ZSH_THEME="bira"
+#export ZSH_THEME="bira"
+export ZSH_THEME="agnoster"
 
 # Set to this to use case-sensitive completion
 # CASE_SENSITIVE="true"
@@ -24,8 +25,11 @@ DISABLE_AUTO_TITLE="true"
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git osx ruby) #bundler) #vi-mode
+plugins=(git osx ruby zsh-syntax-highlighting) #bundler) #vi-mode
 
 source $ZSH/oh-my-zsh.sh
 
 # Customize to your needs...
+
+unsetopt correct
+unsetopt correct_all
