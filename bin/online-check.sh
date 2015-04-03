1ea783a6dda576755c6992bd8a2b5359c69f82a8
Jon Phenow <j.phenow@gmail.com>
2015-04-03 15:21:12 -0500

    shut the fuck up

--- bin/online-check.sh
+++ bin/online-check.sh
@@ -5,7 +5,7 @@
 
 local offline=`dig 8.8.8.8 +time=1 +short google.com A | grep -c "no servers could be reached"`
 if [[ "$offline" == "0" ]]; then
-  rm ~/.offline
+  rm ~/.offline 2> /dev/null
 else
   touch ~/.offline
 fi
