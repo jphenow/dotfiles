f3c6918a8bd6b58d4f6caf0878f81a5ce44806f2
Jon Phenow <jphenow@github.com>
2020-06-26 10:58:58 -0500

    fix terminal for vscode

--- zsh/color.zsh
+++ zsh/color.zsh
@@ -1,10 +1,12 @@
 # enable color support of ls and also add handy aliases
-if [ "$TERM" != "dumb" ]; then
-  #export TERM="xterm-256color"
-  export TERM="screen-256color"
-  export CLICOLOR=1
-  gdircolors -b $DOT/colors/dir_colors &> /dev/null
-fi
+# Caused echo'd commands before executing on vscode terminal
+# if [ "$TERM" != "dumb" ]; then
+#   #export TERM="xterm-256color"
+#   export TERM="screen-256color"
+#   export CLICOLOR=1
+#   gdircolors -b $DOT/colors/dir_colors &> /dev/null
+# fi
+
 # Enable colors for ls, etc.  Prefer $DOT/colors/dir_colors #64489
 if whence dircolors > /dev/null ; then
   if [[ -f $DOT/colors/dir_colors ]] ; then
